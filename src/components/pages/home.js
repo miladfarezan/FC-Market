ac29c5a1014ec70c187dee1d6478629236726f80
milad farezan <miladfarezan@gmail.com>
2023-07-01 11:30:34 +0330

    FC Market

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,14 +3,15 @@ import { Row, Col } from "react-bootstrap";
 import axios from "axios";
 
 import Product from "../product/product";
+import Products from "../../products";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:8000/api/products").then((res) => {
-      setProducts(res.data);
-    });
-  }, []);
+  // const [products, setProducts] = useState([]);
+  // useEffect(() => {
+  //   axios.get("http://localhost:8000/api/products").then((res) => {
+  //     setProducts(res.data);
+  //   });
+  // }, []);
 
   const center = {
     textAlign: "center",
@@ -19,7 +20,7 @@ const Home = () => {
     <div>
       <h2 style={center}>محصولات</h2>
       <Row>
-        {products.map((item) => {
+        {Products.map((item) => {
           return (
             <Col sm={12} md={6} lg={4} key={item._id}>
               <Product product={item} />
--- src/components/pages/product.js
+++ src/components/pages/product.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
+import Products from "../../products";
 
 import "./product.css";
 import axios from "axios";
 
 const Product = () => {
   const params = useParams();
-  const [product, setProduct] = useState({});
-  useEffect(() => {
-    axios.get(`http://localhost:8000/api/products/${params.id}`).then((res) => {
-      setProduct(res.data);
-    });
-  }, []);
+  // const [product, setProduct] = useState({});
+  // useEffect(() => {
+  //   axios.get(`http://localhost:8000/api/products/${params.id}`).then((res) => {
+  //     setProduct(res.data);
+  //   });
+  // }, []);
+  const product = Products.find((item) => {
+    return item._id === params.id;
+  });
 
   return (
     <div>
