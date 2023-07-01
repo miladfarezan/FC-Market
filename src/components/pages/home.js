8041770a1f6bbad0f785945222b4abc48a90e38f
milad farezan <miladfarezan@gmail.com>
2023-07-01 11:23:48 +0330

    main

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import axios from "axios";
 
-import products from "../../products";
 import Product from "../product/product";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/products").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
   const center = {
     textAlign: "center",
   };
--- src/components/pages/product.js
+++ src/components/pages/product.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
 
-import products from "../../products";
-
-function counter() {
-  var count = 0;
-
-  return function counter1() {
-    return count++;
-  };
-}
-
-var c = counter();
-
-console.log(c());
-console.log(c());
-console.log(c());
-console.log(c());
+import "./product.css";
+import axios from "axios";
 
 const Product = () => {
   const params = useParams();
-  const product = products.find((item) => {
-    return item._id === params.id;
-  });
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/products/${params.id}`).then((res) => {
+      setProduct(res.data);
+    });
+  }, []);
 
-  return <div>{product && <div>this</div>}</div>;
+  return (
+    <div>
+      <Link to="/" className="btn btn-light my-3 ">
+        بازگشت به صفحه اصلی
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup.Item variant="flush" className="py-3 product-description">
+            <h3>{product.name}</h3>
+          </ListGroup.Item>
+          <ListGroup.Item variant="flush" className="py-3 product-description">
+            {product.price}
+          </ListGroup.Item>
+          <ListGroup.Item variant="flush" className="py-3 product-description">
+            {product.description}
+          </ListGroup.Item>
+        </Col>
+        <Col md={3}>
+          <Button className="btn-block" type="button">
+            افزودن به سبد خرید
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Product;
